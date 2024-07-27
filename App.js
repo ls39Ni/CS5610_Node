dcc95ee3a0bdd3445e4ba8e1776e999ba21422a8
cliffdawg <cliffordyin@gmail.com>
2024-07-27 03:12:11 -0700

    a6

--- App.js
+++ App.js
@@ -5,10 +5,36 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb+srv://Cluster36578:SVV9SW5wRlBX@cluster36578.rnzdhar.mongodb.net/kanbas?retryWrites=true&w=majority&appName=Cluster36578"
+mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use(cors()); 
+app.use(
+ cors({
+   credentials: true,
+   origin: process.env.NETLIFY_URL || "http://localhost:3000",
+ })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
